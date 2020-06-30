06d9fe0ef9fac23c5dd1375459440f8b40ca165a
Santiago Solana <santiago.sjs@gmail.com>
2020-06-30 18:04:47 -0400

    Simplified the javascript

--- script.js
+++ script.js
@@ -1,25 +1,16 @@
 var generateBtn = document.querySelector("#generate");
 
-
 var passwordlength = parseInt(prompt("How Long Should the password be? choose from 8 to 128 characters"));
-// var useUpperCase = document.;
-// var useNumbers = 
-// var useSpecialCharacters = 
 
 
 if (passwordlength > 7 && passwordlength < 128){
 
-   
-   
     var useUpperCase = confirm("would you like to include uppercase letters?");
     var useNumbers = confirm("would you like to include numbers?");
     var useSpecialCharacters = confirm("would you like to include special characters?");
-    var userLowerCase = true;
-    
+   
     var password = "";
     
-    
- 
     
     function generatePassword() {
         password += String.fromCharCode(Math.floor(Math.random() * 26) + 97);
@@ -35,25 +26,19 @@ if (passwordlength > 7 && passwordlength < 128){
             password += specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
     
         }
-    
     }
     
-    
+
     function writePassword(length) {
     
         for (i = 0; i < length; i++) {
             generatePassword();
         }
-        console.log(password);
         
         var passwordText = document.querySelector("#password");
-        passwordText.value = password.substr(0, length).split('').sort(function(){return 0.5-Math.random()}).join('');
-    
-        
-
+        passwordText.value = password.substr(0, length).split('').sort(function(){return 0.5-Math.random()}).join(''); 
     }
 
-    
 
 } else {
     alert("hey pal we said between 8 to 128 characters...  try again... ")
