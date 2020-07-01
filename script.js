ec7cbe479d6c7efa28df589add010510f86777ca
Santiago Solana <santiago.sjs@gmail.com>
2020-07-01 00:00:47 -0400

    finished adding form elements

--- script.js
+++ script.js
@@ -1,52 +1,59 @@
 var generateBtn = document.querySelector("#generate");
 
-var passwordlength = parseInt(prompt("How Long Should the password be? choose from 8 to 128 characters"));
+var passwordlength = document.getElementById("password-length").value;
+var useUpperCase = document.getElementById("upper-case");
+var useNumbers = document.getElementById("numbers");
+var useSpecialCharacters = document.getElementById("special-characters");
 
 
-if (passwordlength > 7 && passwordlength < 128){
+console.log(useNumbers.value);
 
-    var useUpperCase = confirm("would you like to include uppercase letters?");
-    var useNumbers = confirm("would you like to include numbers?");
-    var useSpecialCharacters = confirm("would you like to include special characters?");
-   
-    var password = "";
-    
-    
-    function generatePassword() {
-        password += String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-    
-        if (useUpperCase) {
-            password += String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+generateBtn.addEventListener("click",function(){
+    passwordlength = document.getElementById("password-length").value;
 
-        } if (useNumbers) {
-            password += String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-
-        } if (useSpecialCharacters) {
-            const specialCharacters = "!@#$%^&*-_=+<>?[]{};:,.|~";
-            password += specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
+    if (passwordlength > 7 && passwordlength < 128){
     
-        }
-    }
+        var password = "";
     
-
-    function writePassword(length) {
-    
-        for (i = 0; i < length; i++) {
-            generatePassword();
-        }
+        function generatePassword() {
+            password += String.fromCharCode(Math.floor(Math.random() * 26) + 97);
         
-        var passwordText = document.querySelector("#password");
-        passwordText.value = password.substr(0, length).split('').sort(function(){return 0.5-Math.random()}).join(''); 
+            if (useUpperCase.checked) {
+                password += String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+    
+            } if (useNumbers.checked) {
+                password += String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+    
+            } if (useSpecialCharacters.checked) {
+                const specialCharacters = "!@#$%^&*-_=+<>?[]{};:,.|~";
+                password += specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
+        
+            }
+            return password;
+        }
+        console.log(useNumbers);
+    
+        function writePassword(length) {
+        
+            for (i = 0; i < length; i++) {
+                generatePassword();
+            }
+            
+            var passwordText = document.querySelector("#password");
+            passwordText.value = password.substr(0, length).split('').sort(function(){return 0.5-Math.random()}).join(''); 
+        }
+    
+        writePassword(passwordlength);
+        password = "";
+    
+    } else {
+        alert("hey pal we said between 8 to 128 characters...  try again... ")
+        return;
     }
 
-
-} else {
-    alert("hey pal we said between 8 to 128 characters...  try again... ")
-    location.reload();
-}
+});
 
 
-generateBtn.addEventListener("click",function(){writePassword(passwordlength);});
 
 
 
